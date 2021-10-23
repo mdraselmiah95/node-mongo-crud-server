8522bd94118f2e83f3a94591efa908b32a31a176
rasel <russelroy15@gmail.com>
2021-10-24 04:08:30 +0600

    WIP on node mongo crud server project

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
+const cors = require("cors");
 const app = express();
 const port = 5000;
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Running my CRUD server ");
