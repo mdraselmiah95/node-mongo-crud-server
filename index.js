5e8290fdbdd08f043b525021e9c95240e7b17479
rasel <russelroy15@gmail.com>
2021-10-24 01:47:28 +0600

    WIP on node mongo crud server project

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const expess = require("express");
-const app = expess();
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
@@ -8,4 +9,17 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log("Running server on port", port);
+});
+
+const uri =
+  "mongodb+srv://mymongodbuser1:<password>@cluster0.dkgxs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("Hitting the database.");
+  client.close();
 });
