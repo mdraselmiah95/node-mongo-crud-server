3ea418579be531b23055427fee3ff971b33ef332
rasel <russelroy15@gmail.com>
2021-10-24 02:41:46 +0600

    WIP on node mongo crud server project

--- index.js
+++ index.js
@@ -4,33 +4,21 @@ const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send("Running my CURN server");
+  res.send("Running my CRUD server ");
 });
 
 app.listen(port, () => {
   console.log("Running server on port", port);
 });
-
 const uri =
-  "mongodb+srv://mymongodbuser1:<password>@cluster0.dkgxs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://mynewproject:9ZAZd6dxvzHN6eok@cluster0.rzfie.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-async function run() {
-  try {
-    await client.connect();
-    const database = client.db("foodMaster");
-    const usersCollection = database.collection("users");
-    // create a document to insert
-    const doc = {
-      title: "Record of a Shriveled Datum",
-      content: "No bytes, no problem. Just insert a document, in MongoDB",
-    };
-    const result = await usersCollection.insertOne(doc);
-    console.log(`A document was inserted with the _id: ${result.insertedId}`);
-  } finally {
-    await client.close();
-  }
-}
-run().catch(console.dir);
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("Hitting the database");
+  client.close();
+});
